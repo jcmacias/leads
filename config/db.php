073190620bc3c89e5ae7f85aaa0aa516447ac6d7
root <orlando@insurancecaredirect.com>
2017-10-12 14:40:10 -0400

    "Change in the configuration"

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=172.17.4.239;dbname=leads',
+    'dsn' => 'mysql:host=localhost;dbname=leads',
     'username' => 'leads',
     'password' => 'leads',
     'charset' => 'utf8',

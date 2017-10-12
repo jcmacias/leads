8f525090ce9367d9b5029e633cc3d5a5e2b6dbec
Julio Cesar <jcmacias05@gmail.com>
2017-10-12 15:52:32 -0400

    Clean urls

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=leads',
-    'username' => 'leads',
-    'password' => 'leads',
-    'charset' => 'utf8',
-];
